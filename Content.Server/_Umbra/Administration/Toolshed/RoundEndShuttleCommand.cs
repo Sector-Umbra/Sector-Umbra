34528da85ac194bc594307c9378a989d1dbf5b7a
Simon <63975668+Simyon264@users.noreply.github.com>
2024-06-21 23:34:02 +0200

    Add admin command to toggle CVar.

---
+++ Content.Server/_Umbra/Administration/Toolshed/RoundEndShuttleCommand.cs
@@ -0,0 +1,26 @@
+﻿using Content.Shared.Administration;
+using Content.Shared.CCVar;
+using Robust.Shared.Configuration;
+using Robust.Shared.Toolshed;
+
+namespace Content.Server.Administration.Toolshed;
+
+[ToolshedCommand, AdminCommand(AdminFlags.Admin)]
+public sealed class RoundEndShuttleCommand : ToolshedCommand
+{
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+
+    [CommandImplementation("enable")]
+    public void Enable([CommandInvocationContext] IInvocationContext ctx)
+    {
+        _cfg.SetCVar(CCVars.CallRoundEndWithEmergencyShuttle, true);
+        ctx.WriteLine("The round end timer will now start upon shuttle landing.");
+    }
+
+    [CommandImplementation("disable")]
+    public void Disable([CommandInvocationContext] IInvocationContext ctx)
+    {
+        _cfg.SetCVar(CCVars.CallRoundEndWithEmergencyShuttle, false);
+        ctx.WriteLine("The round end timer will no longer start upon shuttle landing.");
+    }
+}
---
+++ Resources/Locale/en-US/_Umbra/commands/round-end-shuttle-command.ftl
@@ -0,0 +1,2 @@
+﻿command-description-roundendshuttle-enable = Turns the round end timer on once the shuttle lands. This will not have an affect if the shuttle already launched.
+command-description-roundendshuttle-disable = Turns the round end timer off once the shuttle lands. This will not have an affect if the shuttle already launched.
