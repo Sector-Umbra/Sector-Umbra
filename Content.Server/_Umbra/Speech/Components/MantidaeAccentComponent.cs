a639ad0c929673b2972fb48da58ba4701a18d079
Necroa <viktorsandell2008@gmail.com>
2025-01-27 17:10:40 +0100

    removed audio, pending custom ones i can actually use without being sued

---
+++ Content.Server/_Umbra/Speech/Components/MantidaeAccentComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server.Speech.Components;
+
+/// <summary>
+///     Mantis noises.
+/// </summary>
+[RegisterComponent]
+public sealed partial class MantidaeAccentComponent : Component
+{
+
+}
---
+++ Content.Server/_Umbra/Speech/EntitySystems/MantidaeAccentSystem.cs
@@ -0,0 +1,25 @@
+using System.Text.RegularExpressions;
+using Content.Server.Speech.Components;
+
+namespace Content.Server.Speech.EntitySystems;
+
+public sealed class MantidaeAccentSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<MantidaeAccentComponent, AccentGetEvent>(OnAccent);
+    }
+
+    private void OnAccent(EntityUid uid, MantidaeAccentComponent component, AccentGetEvent args)
+    {
+        var message = args.Message;
+
+        // mantis noises
+        message = Regex.Replace(message, "s+", "x");
+        // LOUD MANTIS NOISES!!
+        message = Regex.Replace(message, "S+", "X");
+
+        args.Message = message;
+    }
+}
