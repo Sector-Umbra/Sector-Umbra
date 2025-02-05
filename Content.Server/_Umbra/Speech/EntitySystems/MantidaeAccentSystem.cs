11e700d7e3ea6ec3b30ffcd543d343094034e1b0
Necroa <viktorsandell2008@gmail.com>
2025-02-05 20:59:32 +0100

    added more custom masks, changes the accent.

--- Content.Server/_Umbra/Speech/EntitySystems/MantidaeAccentSystem.cs
+++ Content.Server/_Umbra/Speech/EntitySystems/MantidaeAccentSystem.cs
@@ -1,25 +1,43 @@
-using System.Text.RegularExpressions;
+using System.Text;
 using Content.Server.Speech.Components;
 
-namespace Content.Server.Speech.EntitySystems;
-
-public sealed class MantidaeAccentSystem : EntitySystem
+namespace Content.Server.Speech.EntitySystems
 {
-    public override void Initialize()
+    public sealed class MantidaeAccentSystem : EntitySystem
     {
-        base.Initialize();
-        SubscribeLocalEvent<MantidaeAccentComponent, AccentGetEvent>(OnAccent);
-    }
+        public override void Initialize()
+        {
+            SubscribeLocalEvent<MantidaeAccentComponent, AccentGetEvent>(OnAccent);
+        }
 
-    private void OnAccent(EntityUid uid, MantidaeAccentComponent component, AccentGetEvent args)
-    {
-        var message = args.Message;
+        public string Accentuate(string message)
+        {
+            // Insert E before every S
+            message = InsertS(message);
+            return message;
+        }
 
-        // mantis noises
-        message = Regex.Replace(message, "s+", "x");
-        // LOUD MANTIS NOISES!!
-        message = Regex.Replace(message, "S+", "X");
+        private string InsertS(string message)
+        {
+            // Replace every new Word that starts with s/S
+            var msg = message.Replace(" s", " x").Replace(" S", " X");
 
-        args.Message = message;
+            // Still need to check if the beginning of the message starts
+            if (msg.StartsWith("s", StringComparison.Ordinal))
+            {
+                return msg.Remove(0, 1).Insert(0, "x");
+            }
+            else if (msg.StartsWith("S", StringComparison.Ordinal))
+            {
+                return msg.Remove(0, 1).Insert(0, "X");
+            }
+
+            return msg;
+        }
+
+        private void OnAccent(EntityUid uid, MantidaeAccentComponent component, AccentGetEvent args)
+        {
+            args.Message = Accentuate(args.Message);
+        }
     }
 }
