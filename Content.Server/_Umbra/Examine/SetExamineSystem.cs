1778017e9c3c93535a920d914c9a9247bd97d291
PursuitInAshes <pursuitinashes@gmail.com>
2025-04-28 10:58:00 -0600

    Add back admin logging under "Action" instead of "Verb"

--- Content.Server/_Umbra/Examine/SetExamineSystem.cs
+++ Content.Server/_Umbra/Examine/SetExamineSystem.cs
@@ -1,13 +1,15 @@
 using Content.Server.Administration;
 using Content.Shared._Umbra.Examine;
 using Content.Shared.Actions;
-// using Content.Shared.Administration.Logs; //Supposedly this is the import for ln 33, but its not working.
+using Content.Shared.Administration.Logs;
+using Content.Shared.Database;
 using Robust.Shared.Player;
 
 namespace Content.Server._Umbra.Examine;
 
 public sealed class SetExamineSystem : SharedSetExamineSystem
 {
+    [Dependency] private readonly ISharedAdminLogManager _adminLog = default!;
     [Dependency] private readonly QuickDialogSystem _quickDialog = default!;
 
     public override void Initialize()
@@ -30,7 +32,7 @@ public sealed class SetExamineSystem : SharedSetExamineSystem
         _quickDialog.OpenDialog(actor.PlayerSession, Loc.GetString("set-examine-title"), setExaminePrompt,
             (string ExamineText) =>
             {
-                // _adminLog.Add(LogType.Verb, $"{ToPrettyString(ent)} set their examine text to {ExamineText}"); // Something here is causing a compile error.
+                _adminLog.Add(LogType.Action, $"{ToPrettyString(ent)} set their examine text to: {ExamineText}");
                 ent.Comp.ExamineText = ExamineText;
                 Dirty(ent);
             });
