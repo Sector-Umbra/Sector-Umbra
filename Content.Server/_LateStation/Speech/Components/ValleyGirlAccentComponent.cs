86f895ea310f586c42ce5c50db213283f4e588e3
TsjipTsjip <19798667+TsjipTsjip@users.noreply.github.com>
2025-05-31 13:58:47 +0000

    Port valley girl accent from LateStation14 (#354)
    * Partial functionality of the valley girl accent. What it currently has is functional, just needs more.
    
    * Improvements; suffix, prefix, like in the middle of the sentence. Functional, just needs more content.
    
    * AUGH.
    
    * A few more word replacements, rewrote a part of the component code for improved readability and performance.
    
    * Tested and functional, ready for in-game use.
    
    * Update valleygirl.ftl
    
    Like, you know~
    
    Signed-off-by: Eirenex <41762755+EirenexTheDragon@users.noreply.github.com>
    
    * Update valleygirl.ftl
    
    Signed-off-by: Eirenex <41762755+EirenexTheDragon@users.noreply.github.com>
    
    * Update valleygirl.ftl
    
    Signed-off-by: Eirenex <41762755+EirenexTheDragon@users.noreply.github.com>
    
    * Update word_replacements.yml
    
    Signed-off-by: Eirenex <41762755+EirenexTheDragon@users.noreply.github.com>
    
    * Update ValleyGirlAccentSystem.cs
    
    Signed-off-by: Eirenex <41762755+EirenexTheDragon@users.noreply.github.com>
    
    * Update word_replacements.yml
    
    Signed-off-by: Eirenex <41762755+EirenexTheDragon@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Eirenex <41762755+EirenexTheDragon@users.noreply.github.com>
    Co-authored-by: Late <latemare@gmail.com>
    Co-authored-by: Eirenex <41762755+EirenexTheDragon@users.noreply.github.com>

---
+++ Content.Server/_LateStation/Speech/Components/ValleyGirlAccentComponent.cs
@@ -0,0 +1,8 @@
+using Content.Server._Latestation.Speech.EntitySystems;
+
+namespace Content.Server._Latestation.Speech.Components;
+
+[RegisterComponent]
+[Access(typeof(ValleyGirlAccentSystem))]
+public sealed partial class ValleyGirlAccentComponent : Component
+{ }
