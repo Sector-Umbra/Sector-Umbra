5d596d40334a5f6369be92e9681eaa90b6a36cfc
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-08-14 16:04:34 +0000

    Accidental discharge (#432)
    * Fix changelog emojis
    
    * Accidental Discharge

--- Content.Shared/Weapons/Ranged/Components/GunComponent.cs
+++ Content.Shared/Weapons/Ranged/Components/GunComponent.cs
@@ -1,4 +1,5 @@
 using System.Numerics;
+using Content.Shared._Moffstation.Weapons.Ranged.Components; // Moffstation
 using Content.Shared.Weapons.Ranged.Events;
 using Content.Shared.Weapons.Ranged.Systems;
 using Robust.Shared.Audio;
@@ -263,6 +264,17 @@ public sealed partial class GunComponent : Component
     /// </summary>
     [DataField]
     public Vector2 DefaultDirection = new Vector2(0, -1);
+
+    // Moffstation - Start - Gun Recoil Kick
+    [DataField]
+    public RecoilKick? RecoilKick;
+
+    /// <summary>
+    /// Odds that the gun will fire upon landing on the ground
+    /// </summary>
+    [DataField]
+    public float FireOnLandChance = 0.1f;
+    // Moffstation - End
 }
 
 [Flags]
